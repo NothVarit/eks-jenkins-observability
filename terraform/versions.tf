1867b0f8db534e614fed1c3ed8aa0a8a1722974b
Varit Srisuphanthong <t.varit.srisuphanthong@TECHXMACI26013A.local>
2026-03-31 11:15:21 +0700

    feat: s3 bucket

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.3.0"
 
+  backend "s3" {
+    bucket = "observability-terraform-state-510485988616"
+    key    = "eks/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
